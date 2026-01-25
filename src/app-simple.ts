c15328072afd0a4c4b2438d57e3b60a3c23c197e
akilapramod <akilapramod@hotmail.com>
2026-01-25 14:07:27 +0530

    update src/app-simple.ts to explicitly allow Cache-Control and Pragma headers.

--- src/app-simple.ts
+++ src/app-simple.ts
@@ -30,7 +30,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Cache-Control', 'Pragma'],
 };
 app.use(cors(corsOptions));
 
